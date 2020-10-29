d4bdc06114819099645b329bf182952a807879fc
Kyle <kaschau89@gmail.com>
2020-10-29 13:25:25 -0400

    Added some better plots to main driver

--- sem.py
+++ sem.py
@@ -10,37 +10,31 @@ U0 = 10 #Bulk flow velocity, used to determine length of box
 tme = 10 #Time of signal, used to determine length of box
 u_tau = 0.1
 delta = np.pi #Defined from flow configuration
-nframes = 100
+nframes = 200
 #Box geobm
 y_height = delta*2.0
 z_width = 2*np.pi
 x_length = U0*tme
 
 #Eddy Density
-C_Eddy = 1.0
+C_Eddy = 0.5
 
 #Initialize domain
-domain = channel(tme,U0,0,y_height,0,z_width,delta,u_tau)
+domain = channel(tme,U0,0,y_height,0,z_width,delta=delta,utau=u_tau)
 
 #Set flow properties from existing data
 domain.set_flow_data(sigmas_from='jarrin',stats_from='moser')
 
 #Populate the domain
-domain.populate()
+domain.populate(C_Eddy)
 
 #Create y,z coordinate pairs for calculation
+ys = np.array([np.pi/3.0])
 ys = np.linspace(0.001*y_height,y_height*0.999,10)
 zs = np.ones(ys.shape[0])*np.pi
 
-#Flucuation array
-primes = np.empty((ys.shape[0],nframes,3))
-
 #Compute u'
-primes[:,:,0] = generate_primes(ys,zs,domain,nframes,'u')
-#Compute v'
-primes[:,:,1] = generate_primes(ys,zs,domain,nframes,'v')
-#Compute w'
-primes[:,:,2] = generate_primes(ys,zs,domain,nframes,'w')
+primes = generate_primes(ys,zs,domain,nframes)
 
 #Compute stats along line
 uus = np.mean(primes[:,:,0]**2,axis=1)
@@ -59,8 +53,8 @@ Us = domain.Ubar_interp(ys) + np.mean(primes[:,:,0],axis=1)
 fig, ax1 = plt.subplots()
 ax1.set_ylabel(r'$y$')
 ax1.set_xlabel(r'$\bar{U}$')
-ax1.plot(Us,ys,label=r'$\bar{U}$ SEM')
-ax1.plot(domain.Ubar_interp(ys),ys,label=r'$\bar{U}$ Moser')
+ax1.plot(Us,ys,label=r'$\bar{U}$ SEM', color='orange')
+ax1.scatter(domain.Ubar_interp(ys),ys,label=r'$\bar{U}$ Moser',color='k')
 ax1.legend()
 plt.savefig('Ubar.png')
 plt.close()
@@ -69,8 +63,8 @@ plt.close()
 fig, ax1 = plt.subplots()
 ax1.set_ylabel(r'$y$')
 ax1.set_xlabel(r'$R_{uu}$')
-ax1.plot(uus,ys,label=r'$R_{uu}$ SEM')
-ax1.plot(domain.Rij_interp(ys)[:,0,0],ys,label=r'$R_{uu}$ Moser')
+ax1.plot(uus,ys,label=r'$R_{uu}$ SEM', color='orange')
+ax1.scatter(domain.Rij_interp(ys)[:,0,0],ys,label=r'$R_{uu}$ Moser', color='k')
 ax1.legend()
 plt.savefig('Ruu.png')
 plt.close()
@@ -79,8 +73,8 @@ plt.close()
 fig, ax1 = plt.subplots()
 ax1.set_ylabel(r'$y$')
 ax1.set_xlabel(r'$R_{vv}$')
-ax1.plot(vvs,ys,label=r'$R_{vv}$ SEM')
-ax1.plot(domain.Rij_interp(ys)[:,1,1],ys,label=r'$R_{vv}$ Moser')
+ax1.plot(vvs,ys,label=r'$R_{vv}$ SEM', color='orange')
+ax1.scatter(domain.Rij_interp(ys)[:,1,1],ys,label=r'$R_{vv}$ Moser', color='k')
 ax1.legend()
 plt.savefig('Rvv.png')
 plt.close()
@@ -89,8 +83,8 @@ plt.close()
 fig, ax1 = plt.subplots()
 ax1.set_ylabel(r'$y$')
 ax1.set_xlabel(r'$R_{ww}$')
-ax1.plot(wws,ys,label=r'$R_{ww}$ SEM')
-ax1.plot(domain.Rij_interp(ys)[:,2,2],ys,label=r'$R_{ww}$ Moser')
+ax1.plot(wws,ys,label=r'$R_{ww}$ SEM', color='orange')
+ax1.scatter(domain.Rij_interp(ys)[:,2,2],ys,label=r'$R_{ww}$ Moser', color='k')
 ax1.legend()
 plt.savefig('Rww.png')
 plt.close()
@@ -99,8 +93,8 @@ plt.close()
 fig, ax1 = plt.subplots()
 ax1.set_ylabel(r'$y$')
 ax1.set_xlabel(r'$R_{uv}$')
-ax1.plot(uvs,ys,label=r'$R_{uv}$ SEM')
-ax1.plot(domain.Rij_interp(ys)[:,0,1],ys,label=r'$R_{uv}$ Moser')
+ax1.plot(uvs,ys,label=r'$R_{uv}$ SEM', color='orange')
+ax1.scatter(domain.Rij_interp(ys)[:,0,1],ys,label=r'$R_{uv}$ Moser', color='k')
 ax1.legend()
 plt.savefig('Ruv.png')
 plt.close()
@@ -109,8 +103,8 @@ plt.close()
 fig, ax1 = plt.subplots()
 ax1.set_ylabel(r'$y$')
 ax1.set_xlabel(r'$R_{uw}$')
-ax1.plot(uws,ys,label=r'$R_{uw}$ SEM')
-ax1.plot(domain.Rij_interp(ys)[:,0,2],ys,label=r'$R_{uw}$ Moser')
+ax1.plot(uws,ys,label=r'$R_{uw}$ SEM', color='orange')
+ax1.scatter(domain.Rij_interp(ys)[:,0,2],ys,label=r'$R_{uw}$ Moser', color='k')
 ax1.legend()
 plt.savefig('Ruw.png')
 plt.close()
@@ -119,8 +113,16 @@ plt.close()
 fig, ax1 = plt.subplots()
 ax1.set_ylabel(r'$y$')
 ax1.set_xlabel(r'$R_{vw}$')
-ax1.plot(vws,ys,label=r'$R_{vw}$ SEM')
-ax1.plot(domain.Rij_interp(ys)[:,1,2],ys,label=r'$R_{vw}$ Moser')
+ax1.plot(vws,ys,label=r'$R_{vw}$ SEM', color='orange')
+ax1.scatter(domain.Rij_interp(ys)[:,1,2],ys,label=r'$R_{vw}$ Moser', color='k')
 ax1.legend()
 plt.savefig('Rvw.png')
+plt.close()
+
+#Sample u prime
+fig, ax1 = plt.subplots()
+ax1.set_ylabel(r'$u \prime$')
+ax1.set_xlabel(r't')
+ax1.plot(np.linspace(0,domain.xmax,nframes),primes[int((primes.shape[0])/2),:,0],color='orange')
+plt.savefig('uprime.png')
 plt.close()
