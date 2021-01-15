99ac3f3d5e0d757755ce4fa9ee38f82908d97e0b
cjohnson407 <chelsea.johnson@gatech.edu>
2021-01-15 14:08:46 -0500

    There were two issues with print statements hanging f's
     Please enter the commit message for your changes. Lines starting

--- utilities/sem4raptor.py
+++ utilities/sem4raptor.py
@@ -71,7 +71,7 @@ domain.compute_sigmas()
 #Print out a summarry
 domain.print_info()
 
-print(f'Generating signal that is {tme} [s] long, with {nframes} frames.\n')
+print('Generating signal that is {tme} [s] long, with {nframes} frames.\n')
 
 ############################
 # RAPTOR
@@ -98,7 +98,7 @@ mb.compute_metrics(xc=False,xu=True,xv=True,xw=True)
 for bn,pn in zip(block_num,patch_num):
     blk = mb[bn-1]
     print('\n*******************************************************************************')
-    print(f'**************************** Working on Patch #{pn} ******************************')
+    print('**************************** Working on Patch #{pn} ******************************')
     print('*******************************************************************************\n')
 
     print('########## Computing Fluctuations ############')
