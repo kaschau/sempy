Fixed bug with incorrect dimensions on u coeff save...
There was an indexing error with the u coeff save out where we only saved
the first three instead of the required four. Also I was adding the
dimensional Ubar to the non dimensional uprime array for save out.

utilities/sem4raptor.py
@@ -121,7 +121,8 @@ for bn,pn in zip(block_num,patch_num):
     face_z = np.concatenate((face_z, blk.zw[ 0,:,0]))
     face_z = np.concatenate((face_z, blk.zw[-1,:,0]))
 
-    primes = sempy.generate_primes(face_y, face_z, domain, nframes, normalization=normalization)
+    u,v,w = sempy.generate_primes(face_y, face_z, domain, frame_num, normalization=normalization)
+    # u.shape = y.shape
     # primes[ nframe , (y,z) pair, (u,v,w)]
 
     #We already have the u fluctuations for ALL the U momentum faces, so we just pull those directly.
@@ -153,7 +154,8 @@ for bn,pn in zip(block_num,patch_num):
     t = np.linspace(0,tme,nframes)
     bc = 'periodic'
     #Add the mean profile here
-    fu = itrp.CubicSpline(t,up+domain.Ubar_interp(blk.yu[:,:,0]),bc_type=bc,axis=0)
+    Upu = domain.Ubar_interp(blk.yu[:,:,0]/inp['refvl']['U_ref'] + up
+    fu = itrp.CubicSpline(t,Upu),bc_type=bc,axis=0)
     fv = itrp.CubicSpline(t,vp,bc_type=bc,axis=0)
     fw = itrp.CubicSpline(t,wp,bc_type=bc,axis=0)
 
@@ -163,6 +165,6 @@ for bn,pn in zip(block_num,patch_num):
     for interval in range(nframes-1):
         file_name = './alphas/alphas_{:06d}_{:03d}'.format(pn,interval+1)
         with FortranFile(file_name, 'w') as f90:
-            f90.write_record(fu.c[:,interval,:].astype(np.float64))
+            f90.write_record(fu.c[:,interval,:,:].astype(np.float64))
             f90.write_record(fv.c[:,interval,:,:].astype(np.float64))
             f90.write_record(fw.c[:,interval,:,:].astype(np.float64))