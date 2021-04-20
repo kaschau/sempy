c47b3ce53bff5671150a27a88ca392bc5545c649
Kyle A. Schau <kaschau89@gmail.com>
2021-04-20 09:00:37 -0400

    Cleaned up shape function plotting.

--- shape_funcs/tent.py
+++ shape_funcs/tent.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def tent(dists,sigmas):
     '''
+
     The legendary tent function. NOTE: distances in dists are not absolute valued.
     They can be positive and negative. Also not all the eddys provided will contribute
     to all velocity components. So you need to zero out manually the ones that dont.
@@ -46,12 +47,12 @@ def tent(dists,sigmas):
 
 if __name__ == '__main__':
 
-    nsig = 101
+    nsig = 201
     u_length_scale = 1.0
     v_length_scale = 0.5
     w_length_scale = 0.25
     sigs = np.empty((nsig,3))
-    line = np.linspace(-u_length_scale,u_length_scale,nsig)
+    line = np.linspace(-u_length_scale*1.5,u_length_scale*1.5,nsig)
     for ii,i in enumerate(line):
         dists = np.array([[i,0,0]])
         sigmas= np.zeros((1,3,3))
@@ -61,7 +62,20 @@ if __name__ == '__main__':
         sigs[ii,:] = tent(dists,sigmas)[0]
 
     import matplotlib.pyplot as plt
-    lo = plt.plot(line,sigs)
-    plt.legend(lo, (f'u={u_length_scale}', f'v={v_length_scale}', f'w={w_length_scale}'))
-    plt.grid()
+    import matplotlib
+    if matplotlib.checkdep_usetex(True):
+        plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    plt.rcParams['figure.figsize'] = (6,4.5)
+    plt.rcParams['figure.dpi'] = 200
+    plt.rcParams['figure.autolayout'] = True
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['lines.linewidth'] = 1.0
+
+    fig,ax = plt.subplots()
+    lo = ax.plot(line,sigs)
+    ax.set_title(r'$f(|x-\sigma_{i}|)$')
+    ax.set_xlabel(r'$x$')
+    plt.legend(lo, (r'$\sigma_{u}$='+f'{u_length_scale}',r'$\sigma_{v}=$'+f'{v_length_scale}',r'$\sigma_{w}=$'+f'{w_length_scale}'))
+    plt.grid(linestyle='--')
     plt.show()
