b8c6e20b5f02b15fc22eaf0de7eeb214757e9af2
Kyle A. Schau <kaschau89@gmail.com>
2021-04-21 09:09:26 -0400

    Fixed bug where eddys werent clipped to zero past major length scale.

--- shape_funcs/blob.py
+++ shape_funcs/blob.py
@@ -39,6 +39,10 @@ def blob(dists,sigmas):
         #Loop over each x,y,z direction, find the largest length scale in that direction,
         # compute the "big" eddy, and then the other two small eddys
         for xyz in range(3):
+            if np.abs(d[xyz]) > np.max(s[:,xyz]):
+                fx[:,xyz] = 0.0
+                break
+
             max_component = np.argmax(s[:,xyz])
             big_eddy = np.cos(np.pi*d[xyz]/(2.0*s[max_component,xyz]))
 
@@ -59,19 +63,22 @@ def blob(dists,sigmas):
 if __name__ == '__main__':
 
     nsig = 201
-    u_length_scale = 1.0
-    v_length_scale = 0.5
-    w_length_scale = 0.25
+    ux_length_scale = 1.0
+    uy_length_scale = 0.75
+    vx_length_scale = 0.5
+    vy_length_scale = 0.25
+    wx_length_scale = 0.25
+    wy_length_scale = 0.25
 
-    line = np.linspace(-u_length_scale*1.5,u_length_scale*1.5,nsig)
+    line = np.linspace(-ux_length_scale*1.5,ux_length_scale*1.5,nsig)
 
     dists = np.zeros((nsig,3))
     dists[:,0] = line
 
-    sigmas= np.zeros((nsig,3,3))
-    sigmas[:,0,:] = u_length_scale
-    sigmas[:,1,:] = v_length_scale
-    sigmas[:,2,:] = w_length_scale
+    sigmas= np.ones((nsig,3,3))
+    sigmas[:,0,0] = ux_length_scale
+    sigmas[:,1,0] = vx_length_scale
+    sigmas[:,2,0] = wx_length_scale
 
     sigs = blob(dists,sigmas)
 
@@ -85,11 +92,60 @@ if __name__ == '__main__':
     plt.rcParams['figure.autolayout'] = True
     plt.rcParams['font.size'] = 14
     plt.rcParams['lines.linewidth'] = 1.0
+    plt.rcParams['image.cmap'] = 'coolwarm'
 
     fig,ax = plt.subplots()
     lo = ax.plot(line,sigs)
     ax.set_title(r'$f(|x-\sigma_{i}|)$')
     ax.set_xlabel(r'$x$')
-    plt.legend(lo, (r'$\sigma_{u}$='+f'{u_length_scale}',r'$\sigma_{v}=$'+f'{v_length_scale}',r'$\sigma_{w}=$'+f'{w_length_scale}'))
+    plt.legend(lo, (r'$\sigma_{u}$='+f'{ux_length_scale}',r'$\sigma_{v}=$'+f'{vx_length_scale}',r'$\sigma_{w}=$'+f'{wx_length_scale}'))
     plt.grid(linestyle='--')
     plt.show()
+
+    ##############################################################
+    # Optional 2D plots
+    ##############################################################
+    plot_2D = True
+    if plot_2D:
+        X,Y = np.meshgrid(line,line)
+        npts = X.ravel().shape[0]
+
+        dists = np.zeros((npts,3))
+        dists[:,0] = X.ravel()
+        dists[:,1] = Y.ravel()
+
+        sigmas= np.ones((npts,3,3))
+        sigmas[:,0,0] = ux_length_scale
+        sigmas[:,0,1] = uy_length_scale
+        sigmas[:,1,0] = vx_length_scale
+        sigmas[:,1,1] = vy_length_scale
+        sigmas[:,2,0] = wx_length_scale
+        sigmas[:,2,1] = wy_length_scale
+
+        sigs = blob(dists,sigmas)
+        fig,ax = plt.subplots()
+        tcf = ax.tricontourf(X.ravel(),Y.ravel(),sigs[:,0], levels=np.linspace(-1,1,31))
+        fig.colorbar(tcf,ticks=np.linspace(-1,1,11))
+        ax.set_title(r'$\sigma_{ux}=$' + f'{ux_length_scale}, '+r'$\sigma_{uy}=$'+f'{uy_length_scale}')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.grid(linestyle='--')
+        plt.show()
+
+        fig,ax = plt.subplots()
+        tcf = ax.tricontourf(X.ravel(),Y.ravel(),sigs[:,1], levels=np.linspace(-1,1,31))
+        cb = fig.colorbar(tcf,ticks=np.linspace(-1,1,11))
+        ax.set_title(r'$\sigma_{vx}$='+f'{vx_length_scale}, ' + r'$\sigma_{vy}=$'+f'{vy_length_scale}')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.grid(linestyle='--')
+        plt.show()
+
+        fig,ax = plt.subplots()
+        tcf = ax.tricontourf(X.ravel(),Y.ravel(),sigs[:,2], levels=np.linspace(-1,1,31))
+        cb = fig.colorbar(tcf,ticks=np.linspace(-1,1,11))
+        ax.set_title(r'$\sigma_{wx}$='+f'{wx_length_scale}, ' + r'$\sigma_{wy}=$'+f'{wy_length_scale}')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.grid(linestyle='--')
+        plt.show()
