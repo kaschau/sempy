2136d0f193bbb81f7fab275bd51ebbc23aea7168
Kyle A. Schau <kaschau89@gmail.com>
2021-12-05 10:59:20 -0500

    Moving to camel case to match peregrine

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from .generate_primes import generate_primes
+from .generatePrimes import generatePrimes
 from . import geometries
 from . import profiles
 from . import sigmas
 from . import stats
 from . import normalization
-from . import shape_funcs
+from . import shapeFuncs
--- domain.py
+++ domain.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 class domain:
-    def __init__(self, Ublk, total_time, delta, utau, viscosity):
+    def __init__(self, Ublk, totalTime, delta, utau, viscosity):
 
-        self.x_length = Ublk * total_time
+        self.xLength = Ublk * totalTime
         self.Ublk = Ublk
         self.delta = delta
         self.utau = utau
@@ -14,92 +14,92 @@ class domain:
         except TypeError:
             self.yp1 = None
 
-        self.flow_type = None
-        self.y_height = None
-        self.z_height = None
+        self.flowType = None
+        self.yHeight = None
+        self.zHeight = None
 
-        self.sigma_interp = None
-        self.sigma_x_min = None
-        self.sigma_x_max = None
-        self.sigma_y_min = None
-        self.sigma_y_max = None
-        self.sigma_z_min = None
-        self.sigma_z_max = None
-        self.V_sigma_min = None
-        self.V_sigma_max = None
+        self.sigmaInterp = None
+        self.sigmaXMin = None
+        self.sigmaXMax = None
+        self.sigmaYMin = None
+        self.sigmaYMax = None
+        self.sigmaZMin = None
+        self.sigmaZMax = None
+        self.vSigmaMin = None
+        self.vSigmaMax = None
 
-        self.Rij_interp = None
-        self.Ubar_interp = None
+        self.rijInterp = None
+        self.ubarInterp = None
 
-        self.eddy_locs = None
+        self.eddyLocs = None
         self.eps = None
         self.sigmas = None
 
-        self.profile_from = None
-        self.sigmas_from = None
-        self.stats_from = None
-        self.eddy_pop_method = None
+        self.profileFrom = None
+        self.sigmasFrom = None
+        self.statsFrom = None
+        self.eddyPopMethod = None
 
         self._neddy = None
 
     @property
     def neddy(self):
         if self._neddy is None:
-            return self.eddy_locs.shape[0]
+            return self.eddyLocs.shape[0]
         else:
             return self._neddy
 
-    def set_sem_data(
+    def setSemData(
         self,
-        sigmas_from="jarrin",
-        stats_from="moser",
-        profile_from="channel",
-        scale_factor=1.0,
+        sigmasFrom="jarrin",
+        statsFrom="moser",
+        profileFrom="channel",
+        scaleFactor=1.0,
     ):
 
-        self.profile_from = profile_from
-        self.sigmas_from = sigmas_from
-        self.stats_from = stats_from
+        self.profileFrom = profileFrom
+        self.sigmasFrom = sigmasFrom
+        self.statsFrom = statsFrom
 
         # MEAN VELOCITY PROFILE
-        if profile_from == "channel":
-            from .profiles.channel import add_profile
-        elif profile_from == "bl":
-            from .profiles.bl import add_profile
-        elif profile_from == "uniform":
-            from .profiles.uniform import add_profile
+        if profileFrom == "channel":
+            from .profiles.channel import addProfile
+        elif profileFrom == "bl":
+            from .profiles.bl import addProfile
+        elif profileFrom == "uniform":
+            from .profiles.uniform import addProfile
         else:
-            raise NameError(f"Unknown profile keyword : {profile_from}")
+            raise NameError(f"Unknown profile keyword : {profileFrom}")
 
         # SIGMAS
-        if sigmas_from == "jarrin":
-            from .sigmas.jarrin_channel import add_sigmas
-        elif sigmas_from == "uniform":
-            from .sigmas.uniform import add_sigmas
-        elif sigmas_from == "linear_bl":
-            from .sigmas.linear_bl import add_sigmas
+        if sigmasFrom == "jarrin":
+            from .sigmas.jarrinChannel import addSigmas
+        elif sigmasFrom == "uniform":
+            from .sigmas.uniform import addSigmas
+        elif sigmasFrom == "linearBL":
+            from .sigmas.linearBL import addSigmas
         else:
-            raise NameError(f"Unknown sigmas keyword : {sigmas_from}")
+            raise NameError(f"Unknown sigmas keyword : {sigmasFrom}")
 
         # STATS
-        if stats_from == "moser":
-            from .stats.moser_channel import add_stats
-        elif stats_from == "spalart":
-            from .stats.spalart_bl import add_stats
-        elif stats_from == "isotropic":
-            from .stats.isotropic import add_stats
+        if statsFrom == "moser":
+            from .stats.moserChannel import addStats
+        elif statsFrom == "spalart":
+            from .stats.spalartBL import addStats
+        elif statsFrom == "isotropic":
+            from .stats.isotropic import addStats
         else:
-            raise NameError(f"Unknown stats keyword : {stats_from}")
+            raise NameError(f"Unknown stats keyword : {statsFrom}")
 
-        add_profile(self)
-        add_sigmas(self, scale_factor)
-        add_stats(self)
+        addProfile(self)
+        addSigmas(self, scaleFactor)
+        addStats(self)
 
-    def compute_sigmas(self):
+    def computeSigmas(self):
         # Compute all eddy sigmas as function of y
-        self.sigmas = self.sigma_interp(self.eddy_locs[:, 1])
+        self.sigmas = self.sigmaInterp(self.eddyLocs[:, 1])
 
-    def generate_eps(self):
+    def generateEps(self):
         # generate epsilons
         self.eps = np.where(
             self.randseed.uniform(low=-1, high=1, size=(self.neddy, 3)) <= 0.0,
@@ -107,12 +107,12 @@ class domain:
             1.0,
         )
 
-    def make_periodic(self, periodic_x=False, periodic_y=False, periodic_z=False):
-        if True in [periodic_x, periodic_y, periodic_z]:
+    def makePeriodic(self, periodicX=False, periodicY=False, periodicZ=False):
+        if True in [periodicX, periodicY, periodicZ]:
             print(
-                "Making domain periodic in {}".format("x" if periodic_x else "")
-                + " {}".format("y" if periodic_y else "")
-                + " {}".format("z" if periodic_z else "")
+                "Making domain periodic in {}".format("x" if periodicX else "")
+                + " {}".format("y" if periodicY else "")
+                + " {}".format("z" if periodicZ else "")
             )
 
         # check if we have epsilons or not
@@ -124,71 +124,70 @@ class domain:
             raise AttributeError(
                 "Please compute your domain's sigmas before making it periodic"
             )
-        if self.sigma_interp is None:
+        if self.sigmaInterp is None:
             raise AttributeError(
-                "Please set your domain's sigma_interp before making it periodic"
+                "Please set your domain's sigmaInterp before making it periodic"
             )
         # Make periodic
-        if periodic_x:
-            keep_eddys = np.where(
-                self.eddy_locs[:, 0] + np.max(self.sigmas[:, :, 0], axis=1)
-                < self.x_length
+        if periodicX:
+            keepEddys = np.where(
+                self.eddyLocs[:, 0] + np.max(self.sigmas[:, :, 0], axis=1)
+                < self.xLength
             )
-            keep_eddy_locs = self.eddy_locs[keep_eddys]
-            keep_eps = self.eps[keep_eddys]
-            periodic_eddys = np.where(
-                (self.eddy_locs[:, 0] + np.max(self.sigmas[:, :, 0], axis=1) > 0.0)
-                & (self.eddy_locs[:, 0] - np.max(self.sigmas[:, :, 0], axis=1) < 0.0)
+            keepEddyLocs = self.eddyLocs[keepEddys]
+            keepEps = self.eps[keepEddys]
+            periodicEddys = np.where(
+                (self.eddyLocs[:, 0] + np.max(self.sigmas[:, :, 0], axis=1) > 0.0)
+                & (self.eddyLocs[:, 0] - np.max(self.sigmas[:, :, 0], axis=1) < 0.0)
             )
-            periodic_eddy_locs = self.eddy_locs[periodic_eddys]
-            periodic_eddy_locs[:, 0] = periodic_eddy_locs[:, 0] + self.x_length
-            periodic_eps = self.eps[periodic_eddys]
-            self.eddy_locs = np.concatenate((keep_eddy_locs, periodic_eddy_locs))
-            self.eps = np.concatenate((keep_eps, periodic_eps))
+            periodicEddyLocs = self.eddyLocs[periodicEddys]
+            periodicEddyLocs[:, 0] = periodicEddyLocs[:, 0] + self.xLength
+            periodicEps = self.eps[periodicEddys]
+            self.eddyLocs = np.concatenate((keepEddyLocs, periodicEddyLocs))
+            self.eps = np.concatenate((keepEps, periodicEps))
             # Update the sigma array if it has been created already
-            self.sigmas = self.sigma_interp(self.eddy_locs[:, 1])
-        if periodic_y:
-            keep_eddys = np.where(
-                self.eddy_locs[:, 1] + np.max(self.sigmas[:, :, 1], axis=1)
-                < self.y_height
+            self.sigmas = self.sigmaInterp(self.eddyLocs[:, 1])
+        if periodicY:
+            keepEddys = np.where(
+                self.eddyLocs[:, 1] + np.max(self.sigmas[:, :, 1], axis=1)
+                < self.yHeight
             )
-            keep_eddy_locs = self.eddy_locs[keep_eddys]
-            keep_eps = self.eps[keep_eddys]
-            periodic_eddys = np.where(
-                (self.eddy_locs[:, 1] + np.max(self.sigmas[:, :, 1], axis=1) > 0.0)
-                & (self.eddy_locs[:, 1] - np.max(self.sigmas[:, :, 1], axis=1) < 0.0)
+            keepEddyLocs = self.eddyLocs[keepEddys]
+            keepEps = self.eps[keepEddys]
+            periodicEddys = np.where(
+                (self.eddyLocs[:, 1] + np.max(self.sigmas[:, :, 1], axis=1) > 0.0)
+                & (self.eddyLocs[:, 1] - np.max(self.sigmas[:, :, 1], axis=1) < 0.0)
             )
-            periodic_eddy_locs = self.eddy_locs[periodic_eddys]
-            periodic_eddy_locs[:, 1] = periodic_eddy_locs[:, 1] + self.y_height
-            periodic_eps = self.eps[periodic_eddys]
-            self.eddy_locs = np.concatenate((keep_eddy_locs, periodic_eddy_locs))
-            self.eps = np.concatenate((keep_eps, periodic_eps))
+            periodicEddyLocs = self.eddyLocs[periodicEddys]
+            periodicEddyLocs[:, 1] = periodicEddyLocs[:, 1] + self.yHeight
+            periodicEps = self.eps[periodicEddys]
+            self.eddyLocs = np.concatenate((keepEddyLocs, periodicEddyLocs))
+            self.eps = np.concatenate((keepEps, periodicEps))
             # Update the sigma array if it has been created already
-            self.sigmas = self.sigma_interp(self.eddy_locs[:, 1])
-        if periodic_z:
-            keep_eddys = np.where(
-                self.eddy_locs[:, 2] + np.max(self.sigmas[:, :, 2], axis=1)
-                < self.z_width
+            self.sigmas = self.sigmaInterp(self.eddyLocs[:, 1])
+        if periodicZ:
+            keepEddys = np.where(
+                self.eddyLocs[:, 2] + np.max(self.sigmas[:, :, 2], axis=1) < self.zWidth
             )
-            keep_eddy_locs = self.eddy_locs[keep_eddys]
-            keep_eps = self.eps[keep_eddys]
-            periodic_eddys = np.where(
-                (self.eddy_locs[:, 2] + np.max(self.sigmas[:, :, 2], axis=1) > 0.0)
-                & (self.eddy_locs[:, 2] - np.max(self.sigmas[:, :, 2], axis=1) < 0.0)
+            keepEddyLocs = self.eddyLocs[keepEddys]
+            keepEps = self.eps[keepEddys]
+            periodicEddys = np.where(
+                (self.eddyLocs[:, 2] + np.max(self.sigmas[:, :, 2], axis=1) > 0.0)
+                & (self.eddyLocs[:, 2] - np.max(self.sigmas[:, :, 2], axis=1) < 0.0)
             )
-            periodic_eddy_locs = self.eddy_locs[periodic_eddys]
-            periodic_eddy_locs[:, 2] = periodic_eddy_locs[:, 2] + self.z_width
-            periodic_eps = self.eps[periodic_eddys]
-            self.eddy_locs = np.concatenate((keep_eddy_locs, periodic_eddy_locs))
-            self.eps = np.concatenate((keep_eps, periodic_eps))
+            periodicEddyLocs = self.eddyLocs[periodicEddys]
+            periodicEddyLocs[:, 2] = periodicEddyLocs[:, 2] + self.zWidth
+            periodicEps = self.eps[periodicEddys]
+            self.eddyLocs = np.concatenate((keepEddyLocs, periodicEddyLocs))
+            self.eps = np.concatenate((keepEps, periodicEps))
             # Update the sigma array if it has been created already
-            self.sigmas = self.sigma_interp(self.eddy_locs[:, 1])
+            self.sigmas = self.sigmaInterp(self.eddyLocs[:, 1])
 
-    def print_info(self):
-        print(f"\nFlow Type: {self.flow_type}")
-        print(f"Domain Length = {self.x_length} [m] ({self.x_length/self.Ublk} [s])")
-        print(f"Domain Height = {self.y_height} [m]")
-        print(f"Domain Width = {self.z_width} [m]")
+    def __repr__(self):
+        print(f"\nFlow Type: {self.flowType}")
+        print(f"Domain Length = {self.xLength} [m] ({self.xLength/self.Ublk} [s])")
+        print(f"Domain Height = {self.yHeight} [m]")
+        print(f"Domain Width = {self.zWidth} [m]")
 
         print("Flow Parameters:")
         print(f"    U_bulk = {self.Ublk} [m/s]")
@@ -197,15 +196,15 @@ class domain:
         print(f"    viscosity(nu) = {self.viscosity} [m^2/s]")
         print(f"    Y+ one = {self.yp1} [m]")
 
-        print(f"Sigmas from {self.sigmas_from}")
-        print(f"Stats from {self.stats_from}")
-        print(f"Profile from {self.profile_from}")
+        print(f"Sigmas from {self.sigmasFrom}")
+        print(f"Stats from {self.statsFrom}")
+        print(f"Profile from {self.profileFrom}")
 
         print("Sigmas (min,max) [m]:")
-        print(f"    x : {self.sigma_x_min},{self.sigma_x_max}")
-        print(f"    y : {self.sigma_y_min},{self.sigma_y_max}")
-        print(f"    z : {self.sigma_z_min},{self.sigma_z_max}")
-        print(f"  Vol : {self.V_sigma_min},{self.V_sigma_max}")
+        print(f"    x : {self.sigmaXMin},{self.sigmaXMax}")
+        print(f"    y : {self.sigmaYMin},{self.sigmaYMax}")
+        print(f"    z : {self.sigmaZMin},{self.sigmaZMax}")
+        print(f"  Vol : {self.vSigmaMin},{self.vSigmaMax}")
 
-        print(f"Eddy population method : {self.eddy_pop_method}")
+        print(f"Eddy population method : {self.eddyPopMethod}")
         print(f"Number of Eddys: {self.neddy}")
